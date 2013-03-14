a370b4741fcda0a76a3cc887c13cc1a0a4899bcc
Olof Larsson <olof@sylt.nu>
2013-03-14 13:54:25 +0100

    Fix plugin.yml indentation typo

--- src/com/massivecraft/massivebooks/Lang.java
+++ src/com/massivecraft/massivebooks/Lang.java
@@ -5,6 +5,7 @@ import com.massivecraft.mcore.util.Txt;
 public class Lang
 {
 	public static final String PERMISSION_DESCRIPTION_COPYCOST_TEMPLATE = "copy a book for the cost %f"; 
+	
 	public static final String ACCEPTED_ITEMS_WRITTEN = "a written book";
 	public static final String ACCEPTED_ITEMS_QUILL = "a book and quill";
 	public static final String ACCEPTED_ITEMS_EITHER = "a written book or a book and quill";
@@ -21,8 +22,5 @@ public class Lang
 	
 	public static final String SUCCESS_AUTHOR = Txt.parse("<g>Set author for item in you hand.");
 	public static final String FAIL_AUTHOR = Txt.parse("<b>Could not set author: %s");
-	
-	
-	
 	
 }
