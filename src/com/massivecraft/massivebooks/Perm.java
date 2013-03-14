e46ad8cf2e73e617f343af4dd6c29d35c13242d5
Olof Larsson <olof@sylt.nu>
2013-03-14 09:27:30 +0100

    Setting up perm enum

---
+++ src/com/massivecraft/massivebooks/Perm.java
@@ -0,0 +1,47 @@
+package com.massivecraft.massivebooks;
+
+import org.bukkit.permissions.Permissible;
+
+import com.massivecraft.mcore.util.PermUtil;
+
+public enum Perm
+{
+	// -------------------------------------------- //
+	// ENUM
+	// -------------------------------------------- //
+	
+	// CMD
+	CMD_MCESS("cmd.book"),
+	
+	// END OF LIST
+	;
+	
+	// -------------------------------------------- //
+	// FIELDS
+	// -------------------------------------------- //
+	
+	public final String node;
+	
+	// -------------------------------------------- //
+	// CONSTRUCT
+	// -------------------------------------------- //
+	
+	Perm(final String permissionNode)
+	{
+		this.node = "massivebooks."+permissionNode;
+    }
+	
+	// -------------------------------------------- //
+	// HAS
+	// -------------------------------------------- //
+	
+	public boolean has(Permissible permissible, boolean informSenderIfNot)
+	{
+		return PermUtil.has(permissible, this.node, informSenderIfNot);
+	}
+	
+	public boolean has(Permissible permissible)
+	{
+		return has(permissible, false);
+	}
+}
