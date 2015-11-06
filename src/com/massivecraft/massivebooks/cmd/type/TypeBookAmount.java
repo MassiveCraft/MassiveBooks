6d1faca4ec215441a0b516c83ddb1342aa3371e3
BuildTools <magnus@puj.dk>
2015-11-06 22:50:35 +0100

    Use correct StringUtils
    The StringUtils we used before was not present in the Spigot API

--- src/com/massivecraft/massivebooks/cmd/type/TypeBookAmount.java
+++ src/com/massivecraft/massivebooks/cmd/type/TypeBookAmount.java
@@ -1,6 +1,6 @@
 package com.massivecraft.massivebooks.cmd.type;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.command.CommandSender;
 
 import com.massivecraft.massivebooks.Lang;
